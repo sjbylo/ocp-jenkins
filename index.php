b23bf63b6b1072198907b040c1f043fc34b373b6
Magnus Glantz <sudo@redhat.com>
2017-03-31 12:56:46 +0200

    asdf

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 6.0. Pod IP: '; ?>
+ <?php echo 'App Version: 7.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
